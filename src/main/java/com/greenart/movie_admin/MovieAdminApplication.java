17e53b0f75e4b75bf4b6b3b31624dbb278e3ffbb
JY <whddbs6753@naver.com>
2022-06-24 11:51:39 +0900

    프로젝트 파일 최초 업로드

---
+++ src/main/java/com/greenart/movie_admin/MovieAdminApplication.java
@@ -0,0 +1,13 @@
+package com.greenart.movie_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenart/movie_admin/MovieAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenart.movie_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
